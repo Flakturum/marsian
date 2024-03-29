8151cf4776d2b90cf7cbde458c26b9c4232dd2c7
Flakturum <albinos.4508@gmail.com>
2024-03-29 17:39:16 +0300

    Initial commit

---
+++ test2.py
@@ -0,0 +1,19 @@
+﻿from requests import get, post
+
+print(post('http://localhost:5000/api/jobs',
+           json={'id': 9, 'job': 'installation of radiation protection', 'team_leader': 1, 'work_size': 45,
+                 'collaborators': '6, 4, 7', 'is_finished': False}).json())  # id=9 exists
+print(post('http://localhost:5000/api/jobs',
+           json={'id': 10, 'job': 'installation of radiation protection'}).json())  # not full list of characters
+print(post('http://localhost:5000/api/jobs').json())  # empty request
+print(post('http://localhost:5000/api/jobs',
+           json={'id': 8,
+                 'job': 'installing a long-distance communication antenna',
+                 'work_size': 5,
+                 'collaborators': '6, 3',
+                 'start_date': '',
+                 'end_date': '',
+                 'is_finished': True,
+                 'team_leader': 4
+                 }).json())  # cool request
+print(get('http://localhost:5000/api/jobs').json())
